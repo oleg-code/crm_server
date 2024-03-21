e0771fa7977788c88ccc53782f3cc46f8db9b2a6
Oleg_G <o39594395@gmail.com>
2024-03-21 15:23:35 +0200

    create a middleware to handle an error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body; 
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All field are required'});
+        next(errorHandler(400, 'All field are required'));
     }
+
 
     const  hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -18,6 +19,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json('Signup successful');
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -26,4 +26,15 @@ app.listen(3001, () => {
 });
 
 app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+}
+)
